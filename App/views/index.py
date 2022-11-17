dd6577b08e8526e9a80104ce93fc898bd960d281
Ved Mahadeo <vedkailash1@gmail.com>
2022-11-16 21:26:43 -0400

    Update index.py

--- App/views/index.py
+++ App/views/index.py
@@ -48,7 +48,7 @@ def index_page():
     # establish cart existence
     cart = None
     if current_user.is_authenticated:
-        cart = get_cart_by_session(current_user.id)
+        cart = get_cart_by_session(str(current_user.id))
         if not cart:
             cart = create_cart(current_user.id)
     else:
